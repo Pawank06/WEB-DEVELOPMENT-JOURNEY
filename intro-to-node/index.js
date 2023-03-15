9e42db522ac492d7721ce6fc907b5ed2b83c71f9
“[firstname <pawan06kumar2003@gmail.com>
2023-03-15 23:17:36 +0530

    learned basic node js

---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+const superheroes = require('superheroes');
+var mySuperHeroName = superheroes.random();
+console.log(mySuperHeroName);
+
+const supervillains = require('supervillains');
+var mySupervillain = supervillains.random();
+console.log(mySupervillain);
