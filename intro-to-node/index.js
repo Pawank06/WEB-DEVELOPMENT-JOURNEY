a5062bc90237e036ede3fd7dfe8f5da9e9ae948c
“[firstname <pawan06kumar2003@gmail.com>
2023-03-15 23:21:45 +0530

    added comments

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,6 +1,10 @@
+// Super hero section
+
 const superheroes = require('superheroes');
 var mySuperHeroName = superheroes.random();
 console.log(mySuperHeroName);
+
+// Super villian section
 
 const supervillains = require('supervillains');
 var mySupervillain = supervillains.random();
