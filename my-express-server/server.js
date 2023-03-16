1f1d89881845b4d9fb78c3f16031621a1e4b1dc1
“[firstname <pawan06kumar2003@gmail.com>
2023-03-16 19:46:45 +0530

    learned basic of express js

---
+++ my-express-server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app =express();
+
+app.get("/", function(request, response){
+    response.send("<h1>Hello world</h1>");
+});
+
+app.listen(3000, function(){
+    console.log("Server started on 3000 port");
+});
