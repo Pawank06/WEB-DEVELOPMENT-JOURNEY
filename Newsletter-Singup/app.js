40f53bd9649ebe7785681b0470d760d4dea81353
“[firstname <pawan06kumar2003@gmail.com>
2023-03-19 22:41:27 +0530

    created fullstack signup page for my newslatter

---
+++ Newsletter-Singup/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html")
+});
+
+app.post("/", function(req,res){
+
+    var firstName = req.body.fname;
+    var lastName = req.body.lname;
+    var email = req.body.ename;
+
+    console.log(firstName,lastName,email);
+
+});
+
+app.listen(3000,function(){
+    console.log("Server running on port 3000");
+});
+
