c032bfe697df4a66d52a6f04b52d506834bbdef9
“[firstname <pawan06kumar2003@gmail.com>
2023-03-24 21:57:20 +0530

    learned about ejs

--- todolist-project/app.js
+++ todolist-project/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
 
 const app = express();
+
+var items = [];
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -9,43 +14,25 @@ app.get("/", function(req, res){
     
 
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    
+    var option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        
-        case 1:
-            day = "Monday";
-            break;
+    var day = today.toLocaleDateString("eng-US", option);
 
-        case 2:
-            day = "Tuesday";
-            break;
 
-        case 3:
-            day = "Wednesday";
-            break;
-        
-        case 4:
-            day = "Thursday";
-            break;
+    res.render("list", {kindOfDay: day, newListItems: items});
 
-        case 5:
-            day = "Friday";
-            break;
+});
 
-        case 6:
-            day = "Saturday";
-            break;   
+app.post("/", function(req,res){
 
-        default:
-            console.log("Error: current day is equal to:" + currentDay);
-            break;
-    }
-    res.render("list", {kindOfDay: day});
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 
 });
 
