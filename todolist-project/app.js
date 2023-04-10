fb954975ad980fa1a18bc7adcb0893f07ca437d4
“[firstname <pawan06kumar2003@gmail.com>
2023-04-10 11:57:11 +0530

    added database to my website

--- todolist-project/app.js
+++ todolist-project/app.js
@@ -1,44 +1,87 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 // const { urlencoded } = require("body-parser");
-const date = require(__dirname + "/date.js");
+// const date = require(__dirname + "/date.js");
 
 // console.log(date());
 
 const app = express();
 
 
-let items = ["Buy Food","Cook Food","Eat Food"];
-let workItems = ["Done work"];
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB');
+const itemsSchema = {
+    name: String
+};
+
+const Item =  mongoose.model("Item", itemsSchema);
+
+const item1 = new Item ({
+    name: "Welcome to your to-do list!"
+});
+const item2 = new Item ({
+    name: "Click the + button to add a new item."
+});
+const item3 = new Item ({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1,item2,item3];
+// Item.insertMany(defaultItems)
+//       .then(function () {
+//         console.log("Successfully saved defult items to DB");
+//       })
+//       .catch(function (err) {
+//         console.log(err);
+//       });
+
 app.get("/", function(req, res){
-    
-    let day = date.getDate();
+
     
 
+    Item.find({})
+      .then(function (foundItems) {
+        if (foundItems == 0) {
+            Item.insertMany(defaultItems)
+                .then(function () {
+                 console.log("Successfully saved defult items to DB");
+            })
+            .catch(function (err) {
+              console.log(err);
+            }); 
+            res.redirect("/");
+        }
+        else{
+            res.render("list", {listTitle: "Today", newListItems: foundItems});
+        }
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
 
-    res.render("list", {listTitle: day, newListItems: items});
+    
+    
+
+    
 
 });
 
 app.post("/", function(req,res){
 
-    let item = req.body.newItem;
+    const itemName = req.body.newItem;
+    const item = new Item({
+        name: itemName
+    });
 
-    if(req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect("/work");
-    }
+    item.save();
+    res.redirect("/");
+
     
-    else{
-        items.push(item);
-        res.redirect("/");
-    }
 
 });
 
